0a61141a98f911228caaa046bf4cd53cf5fee753
moktar <moktar@bglobal.com>
2020-09-14 15:30:37 +0600

    Text added on home page

--- imports/ui/pages/Home.vue
+++ imports/ui/pages/Home.vue
@@ -3,7 +3,7 @@
     <h1 class="text-center">Home</h1>
     <p>
       I have never seen such type of faster app. Thanks to <strong>Gain Media</strong> to assign me this task. I have learnt little about Meteor but it increse my interest to the pick level.
-      I will be very proud of myself if I could get chance to work on it for longer time. 
+      The http less data sending and fetching makes it awesome. I will be very proud of myself if I could get chance to work on it for longer time. 
     </p>
     <p></p>
     <p></p>
